062943169d006433fca41ffa6551bf0890b7e0ae
Ryan Danielson <danielson.ryan@protonmail.ch>
2021-06-06 17:40:56 -0700

    Added asset

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+// screen dimensions
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+
+struct Tcod {
+        root: Root,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Shaken And Stirred In The Dungeons")
+        .init();
+
+    let mut tcod = Tcod { root };
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    
+    false
 }
